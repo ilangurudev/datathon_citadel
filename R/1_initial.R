7bea663d2ac3d0f4df24411bcd9a0c87387b77d4
guru <ilangurudev@gmail.com>
2018-02-10 07:52:45 -0600

    nta_find

--- R/1_initial.R
+++ R/1_initial.R
@@ -1,4 +1,4 @@
-pacman::p_load(tidyverse, rebus, janitor)
+pacman::p_load(tidyverse, rebus, janitor, sp)
 
 uber_2014 <- read_csv("data/uber_trips_2014.csv")
 uber_2015 <- read_csv("data/uber_trips_2015.csv")
@@ -7,12 +7,31 @@ geography <- read_csv("data/geography.csv")
 nta <- read_csv("data/zones.csv")
 
 
-geography %>% 
+geography <- 
+  geography %>% 
   mutate(coordinate = rep(c("lat", "lon"), nrow(.)/2),
          vertice = rep(1:(nrow(.)/2), each = 2)) %>% 
   gather(nta_code, value, -vertice, -coordinate) %>% 
   spread(coordinate, value) %>% 
   arrange(nta_code, vertice) 
+
+
+
+find_nta <- function(lat, lon){
+  
+  
+    locations <- 
+      geography %>% 
+      split(.$nta_code) %>% 
+      map_df(function(x){
+        tibble(inside = point.in.polygon(lat, lon, x$lat, x$lon),
+               nta = x$nta_code[1])
+      })
+    
+    locations %>% filter(inside == 1) %>% pull(nta)
+}
+
+find_nta(lat, lon)
 
 
 
