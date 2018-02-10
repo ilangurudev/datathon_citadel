9601ee82844b4c8f4efd85086b4c12c5a01d4ca9
guru <ilangurudev@gmail.com>
2018-02-10 08:41:27 -0600

    munging ubers

--- R/1_initial.R
+++ R/1_initial.R
@@ -1,11 +1,9 @@
-pacman::p_load(tidyverse, rebus, janitor, sp)
+pacman::p_load(tidyverse, rebus, janitor, sp, lubridate, hms)
 
 uber_2014 <- read_csv("data/uber_trips_2014.csv")
 uber_2015 <- read_csv("data/uber_trips_2015.csv")
-
 geography <- read_csv("data/geography.csv")
 nta <- read_csv("data/zones.csv")
-
 
 geography <- 
   geography %>% 
@@ -14,8 +12,6 @@ geography <-
   gather(nta_code, value, -vertice, -coordinate) %>% 
   spread(coordinate, value) %>% 
   arrange(nta_code, vertice) 
-
-
 
 find_nta <- function(lat, lon){
   
@@ -28,18 +24,28 @@ find_nta <- function(lat, lon){
                nta = x$nta_code[1])
       })
     
-    locations %>% filter(inside == 1) %>% pull(nta)
+    nta <- locations %>% filter(inside == 1) %>% pull(nta)
+    
+    if(length(nta) > 0){
+      nta
+    } else {
+      NA
+    }
 }
 
-find_nta(lat, lon)
+uber_2014 <- 
+  uber_2014 %>% 
+    mutate(pickup_datetime = mdy_hm(pickup_datetime),
+           pickup_date = as.Date(pickup_datetime),
+           pickup_hour = hour(pickup_datetime))
+
+# uber_2015 <- 
+  uber_2015 %>%
+    mutate(nta = )
+    
+
+# mutate(nta = map2_chr(pickup_latitude, pickup_longitude, find_nta))
 
 
+uber_2015 %>% 
 
-# %>% 
-#   group_by(nta_code) %>% 
-#   summarise(lat = mean(lat), 
-#             lon = mean(lon))
-
-
-
-geography
